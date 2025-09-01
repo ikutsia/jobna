e67e1c07c3a4376d2fb891283681b9a82947a2f5
irakli <ikutsia@gmail.com>
2025-09-01 21:58:12 +0400

    Further tweeks on hero section and navigation bar

--- src/App.js
+++ src/App.js
@@ -20,8 +20,11 @@ function HomePage() {
 
             {/* Right side buttons */}
             <div className="flex items-center space-x-4">
+              <span className="text-red-600 font-medium text-sm">
+                Feature coming soon...
+              </span>
               <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
-                Your Dashboard
+                Your AI Powered Job Dashboard
               </button>
               <Link
                 to="/signup"
